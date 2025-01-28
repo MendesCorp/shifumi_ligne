e8de987a9447a6bbcb95dc0a7dc1a5e3e3257447
mendessssss <pricillia.mendes-freire@laplateforme.io>
2025-01-28 15:48:42 +0100

    serv

--- serveur/src/main.c
+++ serveur/src/main.c
@@ -1,1 +1,22 @@
 #include "shifumi_ligne/client/modules/global.h"
+
+int main () {
+
+    int serv_fd = socket (AF_INET, SOCKSTREAM, 0); perror("socket");
+    if(serv_fd == -1) return EXIT_FAILURE;
+
+    int error = 
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
