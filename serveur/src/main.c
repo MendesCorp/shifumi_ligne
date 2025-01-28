2fe9fe006e518ed200d381449597b4abe0687a51
damgin <ant529@outlook.fr>
2025-01-28 15:45:26 +0100

    Update main.c

--- serveur/src/main.c
+++ serveur/src/main.c
@@ -1,1 +1,45 @@
 #include "shifumi_ligne/client/modules/global.h"
+
+int main(){
+char buf[255];
+
+    /**
+     * SOCKET
+     * Je crée le socket serveur
+     */
+    int server_fd = socket(AF_INET,SOCK_STREAM,0);perror("socket");
+    // Si la création échoue je ferme mon programme
+    if(server_fd == -1) return EXIT_FAILURE;
+
+    /**
+    * BIND
+    * Je relie le socket à un port et une ip avec la fonction bind()
+    */
+    struct sockaddr_in server_addr = {
+        .sin_addr.s_addr = INADDR_ANY,
+        .sin_family = AF_INET,
+        .sin_port = htons(SERVER_PORT)
+    };
+    int error = bind(server_fd,(struct sockaddr*)&server_addr,sizeof server_addr);perror("bind");
+    if(error == -1) { close(server_fd); return EXIT_FAILURE; }
+
+
+    /**
+     * LISTEN obligatoire 
+     */
+    error = listen(server_fd,BUFSIZ);perror("listen");
+    if(error == -1) { close(server_fd); return EXIT_FAILURE; }
+
+    printf("Server listen on port : %d\n",SERVER_PORT);
+    
+
+    /**
+     * ACCEPT
+     * J'attend qu'un client se connecter grâce à la fonction bloquante accept()
+     * accept renvoi le client accepté ou -1 en cas d'erreur.
+     */
+    struct sockaddr_in client_addr;
+    socklen_t len;
+    int client_fd = accept(server_fd,(struct sockaddr*)&client_addr,&len);
+    
+    // LE PROGRAMME EST EN PAUSE ET ATTEND UN CLIENT ...
