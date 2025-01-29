75f0234e06ffc818b2c9b9d2e22201086f135b0f
mendessssss <pricillia.mendes-freire@laplateforme.io>
2025-01-29 09:34:07 +0100

    tentative corrections des erreurs

--- serveur/main.c
+++ serveur/main.c
@@ -2,16 +2,16 @@
 
 void * thread_accept(int serv_fd, void* scd_client) {
 
-    int error = listen(serv_fd,BUFSIZ);perror("listen");
-    if(error == -1) { close(serv_fd); return EXIT_FAILURE; }
+    int error = listen(serv_fd, BUFSIZ); perror("listen");
+    if(error == -1) return 0;
 
     printf("Server listen on port : %d\n",SERVER_PORT);
     
     struct sockaddr_in client_addr;
     socklen_t len;
 
-    int client_fd = accept(serv_fd,(struct sockaddr*)&client_addr,&len); perror("accept");
-    if(client_fd == -1) return EXIT_FAILURE;
+    int client_fd = accept(serv_fd, (struct sockaddr*) &client_addr, &len); perror("accept");
+    if(client_fd == -1) return 0;
 }
 
 int main() {
@@ -20,31 +20,33 @@ int main() {
     // Si la création échoue je ferme mon programme
     if(serv_fd == -1) return EXIT_FAILURE;
 
-    struct sockaddr_in server_addr = {
-        .sin_addr.s_addr = INADDR_ANY,
+    struct sockaddr_in serv_addr = {
         .sin_family = AF_INET,
         .sin_port = htons(SERVER_PORT)
+        .sin_addr.s_addr = INADDR_ANY,
     };
 
-    int error = bind(serv_fd,(struct sockaddr*)&server_addr,sizeof server_addr); perror("bind");
-    if(error == -1) { close(serv_fd); return EXIT_FAILURE; }
+    int error = bind(serv_fd, (struct sockaddr*) &serv_addr, sizeof serv_addr); perror("bind");
+    if(error == -1) return EXIT_FAILURE; 
 
-    error = listen(serv_fd,BUFSIZ);perror("listen");
-    if(error == -1) { close(serv_fd); return EXIT_FAILURE; }
+    error = listen(serv_fd, BUFSIZ); perror("listen");
+    if(error == -1) return EXIT_FAILURE; 
 
     printf("Server listen on port : %d\n",SERVER_PORT);
     
     struct sockaddr_in client_addr;
     socklen_t len;
 
-    int client_fd = accept(serv_fd,(struct sockaddr*)&client_addr,&len); perror("accept");
+    int client_fd = accept(serv_fd, (struct sockaddr*) &client_addr, &len); perror("accept");
     if(client_fd == -1) return EXIT_FAILURE;
 
 
-        pthread_t scd_client;
-        pthread_create(&scd_client, NULL, thread_accept, NULL);
-        pthread_join(scd_client, NULL);
+    pthread_t scd_client;
+    pthread_create(&scd_client, NULL, thread_accept, NULL);
+    pthread_join(scd_client, NULL);
         
+
+
 
 
 
